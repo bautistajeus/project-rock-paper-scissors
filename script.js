8698b7826667ea6c004f095eb2cb13479a6ec990
Jeus Bautista <bautista.jeus@gmail.com>
2023-09-17 23:36:08 +0800

    Add loop, shows score and final result

--- script.js
+++ script.js
@@ -5,32 +5,51 @@ function getComputerChoice() {
 }
 
 //Write a function that plays a single round of Rock Paper Scissors.
+let playerScore = 0
+let computerScore = 0
+
 function play(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
     if (playerSelection === computerSelection) {
-        return alert("It's a DRAW!");
+        return alert("It's a DRAW!\nThe score is:   You - " + playerScore + "   Computer - " + computerScore);
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER"){ 
-        return alert("You lose! Paper beats Rock!");
+        computerScore += 1
+        return alert("You lose! Paper beats Rock!\nThe score is:   You - " + playerScore + "   Computer - " + computerScore);
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
-        return alert("You win! Rock beats Scissors!");
+        playerScore += 1
+        return alert("You win! Rock beats Scissors!\nThe score is:   You - " + playerScore + "   Computer - " + computerScore);
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        return alert("You win! Paper beats Rock!");
+        playerScore += 1
+        return alert("You win! Paper beats Rock!\nThe score is:   You - " + playerScore + "   Computer - " + computerScore);
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        return alert("You lose! Scissors beat Paper!");
+        computerScore += 1
+        return alert("You lose! Scissors beat Paper!\nThe score is:   You - " + playerScore + "   Computer - " + computerScore);
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        return alert("You win! Scissors beat Paper!");
+        playerScore += 1
+        return alert("You win! Scissors beat Paper!\nThe score is:   You - " + playerScore + "   Computer - " + computerScore);
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        return alert("You lose! Rock beats Scissors");
+        computerScore += 1
+        return alert("You lose! Rock beats Scissors!\nThe score is:   You - " + playerScore + "   Computer - " + computerScore);
     } else {
         return alert("WRONG INPUT!");
     }
 }
+var rounds = 5;
 function game() {
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Choose from Rock, Paper or Scissors');
+        let playerSelection = prompt('LET\'S PLAY "Rock", "Paper" or "Scissors"\n' + rounds + ' rounds left!' );
         let computerSelection = getComputerChoice();
-        console.log(play(playerSelection, computerSelection));
+        play(playerSelection, computerSelection);
+        rounds -= 1
+    }
+    if (playerScore > computerScore) {
+        alert("You WIN the game!\nFinal score is:   You - " + playerScore + "   Computer - " + computerScore);
+    } else if (playerScore < computerScore) {
+        alert("You LOSE the game!\nFinal score is:   You - " + playerScore + "   Computer - " + computerScore);
+    } else {
+        alert("It's a TIE!\nFinal score is:   You - " + playerScore + "   Computer - " + computerScore);
     }
 }
-game();
+game()
+
